2378f44cb72f18a9cb4905ec9325803b35e8c0cf
Thomas Kinnen <thomas.kinnen@gmail.com>
2010-11-02 22:57:11 +0100

    Started demosaic improvement , still need to finish green correction

--- h1/demosaic.m
+++ h1/demosaic.m
@@ -10,8 +10,8 @@ bmask(2:2:h,2:2:w)=1;
 r=zeros(h,w)+imgbayer.*rmask;
 g=zeros(h,w)+imgbayer.*gmask;
 b=zeros(h,w)+imgbayer.*bmask;
-for y=1:2:h
-    for x=2:2:w
+for y=1:2:h         % Von oben jede 2 Zeile
+    for x=2:2:w     % Ab der 2. Spalte jede 2. Spalte
         if x==2
             r(y,x)=imgbayer(y,x+1);
         elseif x==w
@@ -21,8 +21,18 @@ for y=1:2:h
         end
     end
 end
-for y=2:2:h
-    for x=1:2:w
+for y=2:2:h         % Ab der 2. Zeile jede 2. Zeile
+    for x=1:1:w     % Alle spalten
+        if y+1 <= h
+            r(y,x)=(r(y-1,x)+r(y+1,x))/2;
+        else
+            r(y,x)=r(y-1,x);
+        end
+    end
+end
+
+for y=2:2:h         % Ab der 2. Zeile jede 2. Zeile
+    for x=1:2:w     % Ab der 1. Spalte jede 2. Spalte
         if x==1
             b(y,x)=imgbayer(y,x+1);
         elseif x==w
@@ -33,4 +43,19 @@ for y=2:2:h
     end
 end
 
+for y=1:2:h         % Ab der 1 Zeilen jede 2. Zeile
+    for x=1:1:w
+        if y-1 > 0
+            b(y,x)=(b(y-1,x)+b(y+1,x))/2;
+        else
+            b(y,x)=b(y+1,x);
+        end
+    end
+end
+
+for y=1:2:h
+    for x=2:2:w
+        % Add green here
+    end
+end
 img=cat(3,r,g,b);
